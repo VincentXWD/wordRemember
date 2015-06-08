8ba1fd52d73ae6dfae691f46500a6629e8130bd1
VencentXWD <m13853352137@mail.bistu.edu.cn>
2015-06-08 17:51:09 +0800

    Ver 2.1.5

--- unitTest.cpp
+++ unitTest.cpp
@@ -2,7 +2,7 @@
 
 //*
 #include "mainframe.h"
-
+//#include <functional>
 using namespace std;
 
 int main()
@@ -10,7 +10,9 @@ int main()
 	mainFrame testmode;
 	testmode.CLIwordInit();
 	//__WORDLIST temp = test.getWord(0);
-
+	auto a = "as";
+	auto b = "baba";
+	swap(a, b);
 	return EXIT_FAILURE;	//未正常从mainframe中退出
 
 }
